b4737a375045ddb51304eb5e03915a45f599b0f8
raflimr <raflimuhamadridhwan@gmail.com>
2022-07-27 00:33:38 +0700

    add

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ fetch(url)
      var markerArray4 = []
      var markerArray5 = []
      var markerArray6 = []
+     var markerArray7 = []
 
      //indeks 0 diskip karena header
      for(i=1;i<values.length;i++){
@@ -55,6 +56,8 @@ fetch(url)
          markerArray4.push(L.marker([lat, long], {icon: iconPosko}).bindPopup(`<p><b>${namaTempat}</b><br><a href=https://www.google.com/maps/dir//${namaTempatReplace}/@${lat},${long}>Lokasi</a><br>Keterangan:<br>${formattedString}</p>`).openPopup());
         }else if(label == "sekolah"){
              markerArray5.push(L.marker([lat, long], {icon: iconMasyarakat}).bindPopup(`<p><b>${namaTempat}</b><br><a href=https://www.google.com/maps/dir//${namaTempatReplace}/@${lat},${long}>Lokasi</a><br>Keterangan:<br>${formattedString}</p>`).openPopup());
+        }else if(label == "masyarakat"){
+         markerArray7.push(L.marker([lat, long], {icon: iconMasyarakat}).bindPopup(`<p><b>${namaTempat}</b><br><a href=https://www.google.com/maps/dir//${namaTempatReplace}/@${lat},${long}>Lokasi</a><br>Keterangan:<br>${formattedString}</p>`).openPopup());
         }else{
              markerArray6.push(L.marker([lat, long], {icon: iconMasyarakat}).bindPopup(`<p><b>${namaTempat}</b><br><a href=https://www.google.com/maps/dir//${namaTempatReplace}/@${lat},${long}>Lokasi</a><br>Keterangan:<br>${formattedString}</p>`).openPopup());        }
          //document.getElementById("demo").innerHTML += alamat;
@@ -65,6 +68,7 @@ fetch(url)
      var posko = L.layerGroup(markerArray4);
      var sekolah = L.layerGroup(markerArray5);
      var jembatan = L.layerGroup(markerArray6);
+     var masyarakat = L.layerGroup(markerArray7);
      
        var map = L.map('map', {
           center: [-7.2162751,107.8990084],
@@ -73,7 +77,7 @@ fetch(url)
           maxZoom: 18,
           zoomSnap: 0,
           zoomDelta: 0.25,
-          layers: [banjir, longsor, banjirLongsor, posko, sekolah, jembatan]
+          layers: [banjir, longsor, banjirLongsor, posko, sekolah, jembatan, masyarakat]
        });
 
       var cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
@@ -94,6 +98,7 @@ fetch(url)
      layerControl.addOverlay(posko, "Posko");
      layerControl.addOverlay(jembatan, "Jembatan");
      layerControl.addOverlay(sekolah, "Sekolah");
+     layerControl.addOverlay(masyarakat, "Masyarakat");
   });
 
 
